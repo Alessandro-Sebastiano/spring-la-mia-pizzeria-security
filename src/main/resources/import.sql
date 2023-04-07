e8fe98829e4525ae860a854a5d4f8541feda4448
Alessandro-Sebastiano <alesebastiano15@gmail.com>
2023-04-07 17:45:46 +0200

    review

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -2,11 +2,11 @@ INSERT INTO pizzas_db.pizzas(description, name, price) VALUES('Pizza pomodoro, m
 INSERT INTO pizzas_db.pizzas(description, name, price) VALUES('Pizza pomodoro, mozzarella, salame piccante.', 'Diavola', 7);
 INSERT INTO pizzas_db.pizzas(description, name, price) VALUES('Pizza funghi, salsiccia, mozzarella', 'Boscaiola', 5);
 
-INSERT INTO pizzas_db.users(email, password) VALUES('admin@test.com', '{sha256}admin');
-INSERT INTO pizzas_db.users(email, password) VALUES('user@test.com', '{sha256}user');
+INSERT INTO pizzas_db.users(email, password) VALUES('admin@email.com', '{sha256}passwd');
+INSERT INTO pizzas_db.users(email, password) VALUES('user@email.com', '{sha256}passwd');
 
-INSERT INTO pizzas_db.users(id, name) VALUES('1', 'ADMIN');
-INSERT INTO pizzas_db.users(id, name) VALUES('2', 'USER');
+INSERT INTO pizzas_db.roles(id, name) VALUES(1, 'ADMIN');
+INSERT INTO pizzas_db.roles(id, name) VALUES(2, 'USER');
 
 INSERT INTO pizzas_db.users_roles(user_id, roles_id) VALUES(1, 1);
 INSERT INTO pizzas_db.users_roles(user_id, roles_id) VALUES(2, 2);
