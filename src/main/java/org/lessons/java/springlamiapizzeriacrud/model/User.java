c9032a072e211c7af1b8d3931f0c5a60e653c147
Alessandro-Sebastiano <alesebastiano15@gmail.com>
2023-04-07 16:54:14 +0200

    model-user/role

---
+++ src/main/java/org/lessons/java/springlamiapizzeriacrud/model/User.java
@@ -0,0 +1,55 @@
+package org.lessons.java.springlamiapizzeriacrud.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(nullable = false, unique = true)
+    @Email
+    private String email;
+    @Column(nullable = false)
+    private String password;
+    @ManyToMany(fetch = FetchType.EAGER)
+    private Set<Role> roles;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String username) {
+        this.email = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+
+}
